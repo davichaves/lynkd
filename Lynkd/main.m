3f1a632ed540b0725d665c076bb07228c86c00a9
Davi Chaves <dchaves@usc.edu>
2015-04-04 11:02:14 -0700

    Initial Commit

---
+++ Lynkd/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Lynkd
+//
+//  Created by Davi Chaves on 4/4/15.
+//  Copyright (c) 2015 Davi Chaves. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ LynkdTests/LynkdTests.m
@@ -0,0 +1,40 @@
+//
+//  LynkdTests.m
+//  LynkdTests
+//
+//  Created by Davi Chaves on 4/4/15.
+//  Copyright (c) 2015 Davi Chaves. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface LynkdTests : XCTestCase
+
+@end
+
+@implementation LynkdTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
